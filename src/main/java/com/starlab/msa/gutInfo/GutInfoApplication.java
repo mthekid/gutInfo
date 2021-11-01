6048624262705df832304370596eb19e145217b2
= <=>
2021-11-01 20:01:23 +0900

    Initial commit

---
+++ src/main/java/com/starlab/msa/gutInfo/GutInfoApplication.java
@@ -0,0 +1,13 @@
+package com.starlab.msa.gutInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starlab/msa/gutInfo/GutInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlab.msa.gutInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
