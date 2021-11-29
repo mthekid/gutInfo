754f46b95b3dd890ef28a2fb2742ad3e639b760f
= <=>
2021-11-29 18:16:00 +0900

    타임리프 정적 템플릿 재시작 없이 업로드 설정 완료. cache false로 설정하고 해당 html 페이지를 바뀔때마다 재 컴파일해주면 된다. 박테리아 정보 관련 입력페이지 제작중...

--- src/main/java/com/starlab/msa/gutInfo/controller/HomeController.java
+++ src/main/java/com/starlab/msa/gutInfo/controller/HomeController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/ncs-kit")
+@RequestMapping("/microbiome")
 public class HomeController {
 
-    @GetMapping("/inputPage")
-    public String gotoSeachBoard() {
-        return "inputPage";
+    @GetMapping("/inputpage")
+    public String gotoInputPage() {
+        return "inputpage";
     }
 }
